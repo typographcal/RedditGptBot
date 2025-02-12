ed07bc58ae0d1390b88601ebcbae06345ab21a95
typographcal <typographicaltank@gmail.com>
2025-02-13 01:20:11 +0530

    Add files via upload

---
+++ finalConfirmed.py
@@ -0,0 +1,443 @@
+#!/usr/bin/env python
+import praw
+from dotenv import load_dotenv
+import os
+from openai import OpenAI 
+import sys
+import random
+import time
+import requests
+
+
+
+
+
+load_dotenv()
+
+
+# username=os.getenv('username')
+# client_id=os.getenv('client_id')
+# client_secret=os.getenv('client_secret')
+# password=os.getenv('password')
+
+
+class RedditBot:
+    def __init__(self, reddit):
+        self.reddit = reddit
+        self.replied_file = "replied_to.txt"
+
+
+#Reddit account data
+
+    def usrInfo(username, client_id, client_secret, password):
+        Reddit = praw.Reddit(
+            client_id=client_id,
+            client_secret=client_secret,
+            password=password,
+            user_agent="testscript",
+            username=username,
+        )
+        
+        print("User Info : " + "\033[92m" + "good" + "\033[0m")
+        Reddit.read_only = False
+
+        return Reddit
+
+    # Verifies user
+
+    def usrVerify(reddit, username):
+        if reddit.user.me() != username:
+            sys.exit(1)
+        print(reddit.user.me())
+
+    #response generator using chatgpt
+
+    def response(self, prompt, name):
+        client = OpenAI(
+        api_key=os.getenv("api_key"),
+        )
+        completion = client.chat.completions.create(
+            model="gpt-4o-mini-2024-07-18",
+            messages=[
+                {"role": "system", "content": f"you are human on reddit, you mainly task is to roast {name}. now here is how you are going to know who replied to whom: so the comment starts with level 0 and it's thread is level 1, level 2 and so on, means level 1 replied to level 0, level 2 replied to level 1, level 3 replied to level 2 and so on. But as soon as level 0 starts again you understand it's a new comment in the thread. Look up all the subreddit where user has replied and sometimes you roast taking those subreddit, use dark jokes. don't always roast if something is serious like someone's life is in danger or someone is sad you consult them And if someone's reply or post is in Hindi, sometimes you will also reply in Hinglish but mostly you reply in english. And don't reply too long." },
+                {"role": "user", "content": prompt},
+            ]
+        )
+
+        return completion.choices[0].message.content 
+
+
+
+    def check_image(self, image):
+        client = OpenAI()
+
+        responseImage = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {
+            "role": "user",
+            "content": [
+                {
+                "type": "image_url",
+                "image_url": {
+                    "url": image,
+                },
+                },
+            ],
+            }
+        ],
+        max_tokens=300,
+        )
+
+        return responseImage.choices[0]
+
+
+
+    def gather_comments(self, comment, level=0):
+    # Create a list with the current comment as a dictionary
+        comments_list = [{
+            "level": level,
+            "id": comment.id,
+            "username": comment.author.name if comment.author else "[deleted]",
+            "body": comment.body
+        }]
+
+        # Recursively gather replies
+        for reply in comment.replies:
+            comments_list.extend(self.gather_comments(reply, level + 1))
+
+        return comments_list
+
+
+    def reply_to_new_posts(self):
+        try:
+            print("reply_to_new_post starting......")
+            Info = [{"Author": submission.author.name, "Title": submission.title, "url": submission.url, "id": submission.id} for submission in self.reddit.subreddit("dankindianmemes").hot(limit=10)]
+            
+            chosedTitle  = random.choice(Info)
+            print(f"chosen post : {chosedTitle}")
+            
+            
+            chosedUrl = chosedTitle['url']
+            choseId = chosedTitle['id']
+            choseUser = chosedTitle['Author']
+
+
+            submission = self.reddit.submission(choseId)
+
+            print("Printing comments. \n")
+        
+            submission.comments.replace_more(limit=None)
+            all_comments = []
+            for top_level_comment in submission.comments:
+                all_comments.extend(self.gather_comments(top_level_comment))
+
+            new_comment = []
+            for comment in all_comments:
+                new_comment.append(f"Level {comment['level']} | id: {comment['id']} | Username: {comment['username']} | Comment: {comment['body']} .|| ")
+            
+            print("*" * 50)
+            print("\n")
+            # print(new_comment)
+
+            # chosenId = []
+            randomID = random.choice(all_comments)
+            print(randomID)
+            randomIDFinal = randomID['id']
+            userReplying = randomID['username']
+            
+            if userReplying == 'AutoModerator':
+                return
+
+            elif self.has_already_replied(choseId, randomIDFinal):
+                # print(f"Already replied to: {}")
+                return
+                
+        
+            user_data = self.get_user_subreddits(reddit=self.reddit, username=userReplying)
+
+            image_extension = ['png', 'jpg', 'jpeg']
+
+            if any(ext in chosedUrl for ext in image_extension):
+                print(f"Image present : {chosedTitle['url']}")
+                response = requests.get(chosedTitle['url'])
+                # print(response.headers['Content-Type'])
+                if response.status_code == 200:
+                
+                    image_response = self.check_image(chosedUrl)
+                    # print(image_response)
+                    prompt = f"""
+                    Context:
+                    - OP: {chosedTitle['Author']}
+                    - Post Title: {chosedTitle['Title']}
+                    - Images in the post: {image_response}
+                    - where OP has been active recently and mostly:{user_data}
+                    - Discussion: {new_comment}
+                    - prompt: level - {randomID['level']}
+                    - Replying to: {userReplying}
+
+                    Generate a response that fits this context.
+                    Rules:
+                    - Guidelines: At the end of the paragraph add in the foot - '*I am a bot by the way (still in the testing phase).*'
+
+                    """
+                    # - Bot Personality: {bot_personality}
+                    # - Image: {image_description}
+                    # print(prompt)
+                    submit = self.response(prompt=prompt, name=userReplying)
+                    print(f"Reply - {submit}")
+                    salvation = self.reddit.comment(randomIDFinal)
+                    salvation.reply(submit)
+                    self.log_replied(submission_id=randomIDFinal, comment_id=randomIDFinal)
+
+                else:
+                    print(f"status code {response.status_code}")
+
+            # Check if it's a gallery
+            elif submission.url.startswith('https://www.reddit.com/gallery/'):
+                print("Fetching gallery data...")
+                submission = self.reddit.submission(id=choseId)
+                image_response = self.check_image(chosedUrl)
+                for item in submission.gallery_data['items']:
+                    media_id = item['media_id']
+                    image_url = f"https://i.redd.it/{media_id}.jpg"
+                    print(f"Image URL: {image_url}")
+                    prompt = f"""
+                        Context:
+                        - OP: {chosedTitle['Author']}
+                        - Post Title: {chosedTitle['Title']}
+                        - Images in the post: {image_response}
+                        - where OP has been active recently and mostly:{user_data}
+                        - Discussion: {new_comment}
+                        - prompt: level - {randomID['level']}
+                        - Replying to: {userReplying}
+
+                        Generate a response that fits this context.
+                        Rules:
+                        - Guidelines: At the end of the paragraph add in the foot - '*I am a bot by the way (still in the testing phase)*.
+                        """
+                    print(f"User replying to: {userReplying}")
+                    submit = self.response(prompt=prompt, name=userReplying)    
+                    print(submit)
+                    salvation = self.reddit.comment(randomIDFinal)
+                    salvation.reply(submit)
+                    self.log_replied(submission_id=randomIDFinal, comment_id=randomIDFinal)
+                            
+            else:
+                Title = chosedTitle['Title']
+                # image_response = self.check_image(chosedUrl)
+                prompt = f"""
+                        Context:
+                        - OP: {chosedTitle['Author']}
+                        - Post Title: {chosedTitle['Title']}
+                        - where OP has been active recently and mostly:{user_data}
+                        - Discussion: {new_comment}
+                        - prompt: level - {randomID['level']}
+                        - Replying to: {userReplying}
+
+                        Generate a response that fits this context.
+                        Rules:
+                        - Guidelines: At the end of the paragraph add in the foot - '*I am a bot by the way (still in the testing phase)*.
+                        """
+                submit = self.response(prompt=prompt, name=userReplying)
+                print(submit)
+                salvation = self.reddit.comment(randomIDFinal)
+                salvation.reply(submit)
+                self.log_replied(submission_id=randomIDFinal, comment_id=randomIDFinal)
+
+        except Exception as e:
+                print(f"Error posting to new post: {e}")
+        
+        # print("reply_to_new_post closed")
+
+
+
+    # Checks where user who replied to your post has been commented
+    def get_user_subreddits(self, reddit, username):
+        user = reddit.redditor(username)
+        subreddits = [] # Use a set to avoid duplicates
+
+        try:
+            # Check subreddits where the user has submitted posts
+            for submission in user.submissions.new(limit=200):  # Adjust the limit as needed
+                subreddits.append(submission.subreddit.display_name)
+
+            # Check subreddits where the user has commented
+            for comment in user.comments.new(limit=200):  # Adjust the limit as needed
+                subreddits.append(comment.subreddit.display_name)
+
+        except Exception as e:
+            print(f"Error fetching data for user {username}: {e}")
+        
+        return subreddits
+
+
+    def check_mentions(self, reddit, userName):
+            try:
+                print("check_metions starting....")
+                mentions = self.reddit.inbox.unread(limit=None) or self.reddit.inbox.mentions(limit=None)
+                for mention in mentions:
+                    if not mention.author.name == "AutoModerator":
+                        mention.mark_read()
+                        continue
+
+                    submission = mention.submission
+                    submission_id = mention.submission.id
+                    comment_id = mention.id
+
+                    if self.has_already_replied(submission_id, comment_id):
+                        print(f"Already replied to: {mention.context}")
+                        continue
+
+
+
+                    submission.comments.replace_more(limit=None)
+                    # all_comment = submission.comments.list()
+                    postDiscription = submission.selftext
+
+                    mentionTitle = mention.submission.title
+                    whoMention = mention.author.name
+                    mentionSubreddit = mention.subreddit.display_name
+                    mentionText = mention.body
+                    mentionUrl = mention.context
+
+                    op_name = submission.author.name
+
+                    # submission.comments.replace_more(limit=None)
+                    all_comments = []
+                    for top_level_comment in submission.comments:
+                        all_comments.extend(self.gather_comments(top_level_comment))
+
+                    new_comment = []
+                    for comment in all_comments:
+                        new_comment.append(f"Level {comment['level']} | Username: {comment['username']} | Comment: {comment['body']} | id: {comment['id']} .")
+                    
+
+                    user_commented = self.get_user_subreddits(reddit=reddit, username=whoMention)
+                    prompt = f"""
+                    Context:
+                    - Post Title: {mentionTitle}
+                    - OP: {op_name}
+                    - where OP has been active recently and mostly:{user_commented}
+                    - Post Description: {postDiscription}
+                    - Subreddit: {mentionSubreddit}
+                    - Discussion: {new_comment}
+                    - prompt: {mentionText}
+                    - Replying to: {whoMention}
+                    - url of the post: {mentionUrl}
+
+                    Generate a response that fits this context.
+                    Rules:
+                    - Guidelines: At the end of the paragraph add in the foot - '*I am a bot by the way (still in the testing phase)*.
+                    """
+                    # Rules:
+                    # - Bot Personality: {bot_personality}
+                    # - Guidelines: {bot_guidelines}
+                    # - Image: {image_description}
+                    # print(prompt)
+
+                    user_name = whoMention
+                
+                    user_name1 = list(user_name)
+                    user_name2 = user_name1[2:]
+                    user_nameFinal = ''.join(user_name2)
+                    print(user_nameFinal)
+                    ai_response = self.response(prompt=prompt, name=user_nameFinal)
+                    print(ai_response)
+
+                    mention.reply(ai_response)
+                    mention.mark_read()
+                    self.log_replied(submission_id, comment_id)
+            except Exception as e:
+                print(f"Error fetching mentions: {e}")
+
+        
+
+
+    def has_already_replied(self, submission_id, comment_id):
+        # Check if the post/comment ID is in the log file
+        if os.path.exists(self.replied_file):
+            with open(self.replied_file, "r") as file:
+                replied_ids = file.read().splitlines()
+            if comment_id in replied_ids:
+                return True
+        return False
+
+    def log_replied(self, submission_id, comment_id):
+        # Log the submission and comment IDs to the file
+        with open(self.replied_file, "a") as file:
+            file.write(submission_id + "\n")
+            if comment_id is not None:
+                file.write(comment_id + "\n")
+
+
+
+
+
+
+def main():
+    while True:
+        try:
+            # Load environment variables
+
+
+            load_dotenv()
+
+            username = os.getenv('username')
+            client_id = os.getenv('client_id')
+            client_secret = os.getenv('client_secret')
+            password = os.getenv('password')
+
+            # Initialize Reddit instance
+            reddit = RedditBot.usrInfo(username, client_id, client_secret, password)
+
+            # Verify user
+            RedditBot.usrVerify(reddit, username)
+
+            # Create an instance of the RedditBot class
+            bot = RedditBot(reddit)
+            
+            # Delay setup
+            delay_one = 5  
+            delay_two = 2  
+            last_time_one = time.time()
+            last_time_two = time.time()
+
+            while True:
+                current_time = time.time()
+
+                # Call `reply_to_new_posts()` if the required delay has passed
+                if current_time - last_time_one >= delay_one:
+                    try:
+                        bot.reply_to_new_posts()
+                        time.sleep(10)
+                    except Exception as e:
+                        print(f"Error in replying to new posts: {e}")
+                    last_time_one = current_time  # Update the last execution time
+
+                # Call `check_mentions()` if the required delay has passed
+                if current_time - last_time_two >= delay_two:
+                    try:
+                        bot.check_mentions(reddit, username)
+                    except Exception as e:
+                        print(f"Error in checking mentions: {e}")
+                    last_time_two = current_time
+
+
+                # Optionally, add a short sleep to prevent rapid looping
+                print("wait 2 seconds")
+                time.sleep(2)  # Adjust as needed
+            
+
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+        except KeyboardInterrupt:
+            print("Loop interrupted by user.")
+            break
+
+if __name__ == "__main__":
+    main()
+
+
+
+
